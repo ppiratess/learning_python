29a3a6696bcd567fccab61df110d266cc0436193
shubham-tam <tamrakarshubham23@gmail.com>
2025-03-20 20:30:50 +0545

    feat: defaultdict solution

--- builtin_functions/defaultdict.py
+++ builtin_functions/defaultdict.py
@@ -1,10 +1,22 @@
-letters = {}
+from collections import defaultdict
+
+
+# letters = {}
+
+# for letter in "Mississippi":
+#     if letter not in letters:
+#         letters[letter] = 1
+
+#     else:
+#         letters[letter] += 1
+
+# print(letters)
+
+
+# ? solution using default dict
+letters = defaultdict(int)
 
 for letter in "Mississippi":
-    if letter not in letters:
-        letters[letter] = 1
-
-    else:
-        letters[letter] += 1
+    letters[letter] += 1
 
 print(letters)
