ba149e391792446176cb519c63d378ce19b81d31
shubham-tam <tamrakarshubham23@gmail.com>
2025-03-20 20:38:25 +0545

    feat: update anagram solution with defaultdict

--- DSA/anagram.py
+++ DSA/anagram.py
@@ -1,12 +1,40 @@
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         if len(s) != len(t):
+#             return False
+
+#         lookup = {}
+
+#         for letter in s:
+#             lookup[letter] = lookup.get(letter, 0) + 1
+
+#         for letter in t:
+#             if letter not in lookup or lookup[letter] == 0:
+#                 return False
+#             lookup[letter] -= 1
+
+#         return True
+
+
+# solution = Solution()
+
+# print(solution.isAnagram("ccbc", "bbcc"))
+
+
+from collections import defaultdict
+
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
 
-        lookup = {}
+        lookup = defaultdict(int)
 
         for letter in s:
-            lookup[letter] = lookup.get(letter, 0) + 1
+            lookup[letter] += 1
+
+        print(lookup)
 
         for letter in t:
             if letter not in lookup or lookup[letter] == 0:
@@ -18,4 +46,4 @@ class Solution:
 
 solution = Solution()
 
-print(solution.isAnagram("ccbc", "bbcc"))
+print(solution.isAnagram("ccbc", "cbcc"))
